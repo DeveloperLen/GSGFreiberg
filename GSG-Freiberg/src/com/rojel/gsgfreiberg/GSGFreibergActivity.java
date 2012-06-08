80ca4101f265a8e105f957e4a8f7fa12dea23745
Robert Heedt <rojet601@googlemail.com>
2012-06-08 15:52:45 +0200

    Added new apk in GSG-Freiberg/GSG-Freiberg.apk

--- GSG-Freiberg/src/com/rojel/gsgfreiberg/GSGFreibergActivity.java
+++ GSG-Freiberg/src/com/rojel/gsgfreiberg/GSGFreibergActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.widget.*;
 
 public class GSGFreibergActivity extends Activity {
-    /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

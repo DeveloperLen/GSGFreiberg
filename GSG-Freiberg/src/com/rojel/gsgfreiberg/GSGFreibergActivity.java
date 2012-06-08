08c3133dbce3a32b4bca7d2d38e2f7c669b8f215
Robert Heedt <rojet601@googlemail.com>
2012-06-08 15:45:38 +0200

    Reformated the schedule

--- GSG-Freiberg/src/com/rojel/gsgfreiberg/GSGFreibergActivity.java
+++ GSG-Freiberg/src/com/rojel/gsgfreiberg/GSGFreibergActivity.java
@@ -18,11 +18,38 @@ public class GSGFreibergActivity extends Activity {
         Schedule schedule = HTMLHandler.parse(page);
         
         for(int i = 0; i < schedule.size(); i++) {
-	        TextView cancel = new TextView(this);
-	        cancel.setText(schedule.get(i).date + "-" + schedule.get(i).classname + "-" + schedule.get(i).lesson);
+	        TextView date = new TextView(this);
+	        date.setPadding(0, 0, 10, 10);
+	        date.setText(schedule.get(i).date);
+	        
+	        TextView classname = new TextView(this);
+	        classname.setPadding(0, 0, 10, 10);
+	        classname.setText(schedule.get(i).classname);
+	        
+	        TextView lesson = new TextView(this);
+	        lesson.setPadding(0, 0, 10, 10);
+	        lesson.setText(schedule.get(i).lesson);
+	        
+	        TextView newSubject = new TextView(this);
+	        newSubject.setPadding(0, 0, 10, 10);
+	        newSubject.setText(schedule.get(i).newSubject);
+	        
+	        TextView newTeacher = new TextView(this);
+	        newTeacher.setPadding(0, 0, 10, 10);
+	        newTeacher.setText(schedule.get(i).newTeacher);
+	        
+	        TextView room = new TextView(this);
+	        room.setPadding(0, 0, 10, 10);
+	        room.setText(schedule.get(i).room);
 	        
 	        TableRow row = new TableRow(this);
-	        row.addView(cancel);
+	        
+	        row.addView(date);
+	        row.addView(classname);
+	        row.addView(lesson);
+	        row.addView(newSubject);
+	        row.addView(newTeacher);
+	        row.addView(room);
 	        
 	        table.addView(row);
         }
